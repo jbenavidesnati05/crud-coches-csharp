2dca764fb6a01b1c97f3a408bd03ca3bcf17fda3
jbenavides <jbenavides@sistecredito.com>
2023-09-28 10:31:59 -0500

    first commit

---
+++ Concesionario.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_carro
+{
+    internal class Concesionario
+    {
+        private Carro[] _carros;
+        private int _limite;
+        private int _numCarros;
+
+        public Concesionario(int limite)
+        {
+            this._carros = new Carro[limite];
+            _limite = limite;
+            _numCarros = 0;
+        }
+
+        public void anadirCarro(Carro c) 
+        {
+            if (c != null && _numCarros < _carros.Length)
+            {
+                _carros[_numCarros] = c;
+                _numCarros++;
+            }
+        }        
+        public void mostrarCarro() 
+        {
+            for (int i = 0; i< _numCarros; i++) 
+            {
+                Console.WriteLine(_carros[i].ToString());
+            }
+        }        
+        public void vaciarCarro() 
+        {
+            this._carros = new Carro[_limite];
+            _numCarros = 0;
+        }      
+        public void eliminarCarro(Carro c) 
+        {
+            if (c != null && _numCarros != 0)
+            {
+                int posicion = -1;
+                for (int i = 0; i < _numCarros; i++)
+                {
+                    if (c.id == _carros[i].id)
+                    {
+                      posicion = i;
+                    }
+                }
+                if (posicion == -1)
+                {
+                    Console.WriteLine("el coche no existe");
+                }
+                else
+                {
+                    _carros[posicion] = null;
+                    for (int i = posicion; i < _numCarros; i++) 
+                    {
+                        _carros[i] = _carros[i + 1];
+                    }
+                    _numCarros--;
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Poo_carro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Concesionario consecionario = new Concesionario(10);
+
+            Carro c1 = new Carro(1,"bmw","1992",56,5892);
+            Carro c2 = new Carro(2,"ford","2025",34,5892);
+            Carro c3 = new Carro(3,"set","2025",54,5892);
+            Carro c4 = new Carro(4,"chev","2302",67,5892);
+            Carro c5 = new Carro(5,"peugout","4520",23,5892);
+
+
+            Console.WriteLine("Añadiendo todos los coches");
+            consecionario.anadirCarro(c1);
+            consecionario.anadirCarro(c2);
+            consecionario.anadirCarro(c3);
+            consecionario.anadirCarro(c4);
+            consecionario.anadirCarro(c5);
+            Console.WriteLine();
+
+            Console.WriteLine("mostrando todos los coches");
+            consecionario.mostrarCarro();
+            Console.WriteLine();
+
+            Console.WriteLine("elimino dos de  los coches");
+            consecionario.eliminarCarro(c5);
+            consecionario.eliminarCarro(c1);
+            Console.WriteLine();
+
+            Console.WriteLine("muestro nuevamente los coches");
+            consecionario.mostrarCarro();
+            consecionario.eliminarCarro(c1);
+            Console.WriteLine();
+
+            Console.WriteLine("agrego nuevamente dos de  los coches");
+            consecionario.anadirCarro(c5);
+            consecionario.anadirCarro(c1);
+            Console.WriteLine();
+
+            Console.WriteLine("mostrando todos los coches");
+            consecionario.mostrarCarro();
+            Console.WriteLine();
+
+            Console.WriteLine("vaceo todos los choches");
+            consecionario.vaciarCarro();
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
